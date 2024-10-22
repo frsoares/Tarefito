e42b5346cefbaf4f8aac1583e593b7312fbacac7
Francisco Soares <fmssn@cin.ufpe.br>
2024-10-22 07:05:40 -0300

    Initial Commit

---
+++ Tarefito/TarefitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TarefitoApp.swift
+//  Tarefito
+//
+//  Created by Francisco Miranda Soares on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TarefitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
